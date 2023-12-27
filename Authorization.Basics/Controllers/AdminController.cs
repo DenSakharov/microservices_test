cbea6b324bbe10f472ce8c07eb16ba9eaac24579
DeVSakharov <sakhirsp@gmail.com>
2023-12-27 16:04:49 +0300

    Begin implement authorization cookie

---
+++ Authorization.Basics/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Basics.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View();
+        }
+    }
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Authorization.Basics/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Cookie").AddCookie("Cookie",config=>config.LoginPath="/Admin/Login");
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+//сначала этот
+app.UseRouting();
+//
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
